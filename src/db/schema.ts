326071d88a1621f63ec81eea433756f8207d1633
aslpooht <thontrapoowadol@gmail.com>
2025-09-01 10:12:03 +0700

    .

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,11 @@
+import { table } from "console";
+import { uuid } from "drizzle-orm/pg-core";
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+
+export const usersTable = pgTable("users_table",{
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  username: varchar("username").notNull(),
+  email: varchar("email").notNull().unique(),
+  password: varchar("password").notNull()
+})
