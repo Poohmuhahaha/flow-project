517803e547110d31c7f49963a9b2acc5905e4608
aslpooht <thontrapoowadol@gmail.com>
2025-09-01 23:18:21 +0700

    เอาใหม่ ๆ เมื่อกี้พลาด

--- src/app/api/auth/login/page.tsx
+++ src/app/api/auth/login/page.tsx
@@ -1,7 +1,6 @@
 import { LoginForm } from "@/components/auth/login-form"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from "next/link"
-import Image from "Next/Image"
 
 export default function LoginPage() {
   return (
